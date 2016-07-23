e96d1749fd294c0fba863efb657f2fa092ce8dc4
CodyHulsey <sendcodyamessage@live.com>
2016-07-23 14:39:02 -0700

    I removed my portfolio file and placed my portfolio content in the index.html

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
-var rawData = [
+var projectData = [
   {
-    title: 'TEST'
-    category: 'jQuery'
-    publishedOn: '07/19/2017'
-    image: ''
-    body: 'Blah Blah Blah Blah Blah Blah'
+    title: 'Future Hendrix',
+    category: 'jQuery',
+    publishedOn: '07/19/2016',
+    body: '<p>Ayvadius DeMun Wilburn (born November 20, 1983), known professionally by his stage name Future, is an American hip hop recording artist. Born and raised in Atlanta, Georgia, Wilburn first became involved in music as part of the Dungeon Family collective, where he received the nickname the Future. After amassing a series of mixtapes between 2010 and 2011, Future signed a major-label deal with Epic Records and A1 Recordings, which helped launch Future\'s own label imprint, Freebandz.</p><img src="http://www.turntuptracks.com/wp-content/uploads/2014/03/Future-Astronaut-Suite.jpg">',
   },
 ]
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,9 +1,9 @@
 var projects = [];
 
-function project (opts) {
+function Project (opts) {
   this.title = opts.title;
   this.category = opts.category;
-  this.publishedOn: opts.publishedOn;
+  this.publishedOn = opts.publishedOn;
   this.image = opts.image;
   this.body = opts.body;
 }
@@ -13,11 +13,29 @@ Project.prototype.toHtml = function() {
 
   $newProject.attr('data-category', this.category);
 
-  $newProject.find('h2'.html(this.title);
+  $newProject.find('h2').html(this.title);
+
+  $newProject.find('.project-body').html(this.body);
 
   $newProject.find('time[pubdate]').attr('title',this.publishedOn);
 
-  $newProject.find()
-}
+  $newProject.find('time').html(' about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
-//WORK IN PROGRESS
+  $newProject.append('<hr>');
+
+  $newProject.removeClass('template');
+
+  return $newProject;
+}
+//rawData.sort sorts stuff. It's giving you some flexibility on how you want something sorted. A callback is a function as an argument. It's expecting two parameters. We are just doing math. We are comparing the two. .sort is an interation tool. .sort is just an array method.
+projectData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+//Another array method that has another call back. For the length of this array, do something with this element. articles.push pushes that object into the articles array.
+projectData.forEach(function(ele) {
+  projects.push(new Project(ele));
+})
+
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml())
+});
