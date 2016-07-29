ff44209b2e3ebc4a05a19021063d51e113d6fb14
CodyHulsey <sendcodyamessage@live.com>
2016-07-28 20:59:52 -0700

    Handlebars is working. Now I need to focus on SMACCS.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -10,34 +10,28 @@ function Project (opts) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
 
-  $newProject.attr('data-category', this.category);
-  $newProject.attr('data-category', this.about);
-  $newProject.find('data-about', this.about);
-  $newProject.find('h2').html(this.title);
+  var projectTemplate = $('#article-template').html();
 
-  $newProject.find('.project-body').html(this.body);
+  var theProject = Handlebars.compile(projectTemplate);
 
-  $newProject.find('time[pubdate]').attr('title',this.publishedOn);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  $newProject.find('time').html(' about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  var theCompiledHtml = theProject(this);
 
-  $newProject.append('<hr>');
+  $('#projectTemplate').html(theCompiledHtml);
 
-  $newProject.removeClass('template');
+};
 
-  return $newProject;
-}
-//rawData.sort sorts stuff. It's giving you some flexibility on how you want something sorted. A callback is a function as an argument. It's expecting two parameters. We are just doing math. We are comparing the two. .sort is an interation tool. .sort is just an array method.
 projectData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
-//Another array method that has another call back. For the length of this array, do something with this element. articles.push pushes that object into the articles array.
+
 projectData.forEach(function(ele) {
   projects.push(new Project(ele));
 })
 
-projects.forEach(function(a){
+projects.forEach(function(a) {
   $('#projects').append(a.toHtml())
 });
