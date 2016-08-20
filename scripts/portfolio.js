e3a2145b1871bb17d9a48b7393b9508dc21c8ce7
CodyHulsey <sendcodyamessage@live.com>
2016-08-20 10:29:06 -0700

    Plugging away on this assignment. Can't seem to get projects to append to the projects section.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,62 +1,103 @@
-function Project (opts) {
-  this.title = opts.title;
-  this.about = opts.about;
-  this.category = opts.category;
-  this.source = opts.source;
-  this.sourceUrl = opts.sourceUrl;
-  this.publishedOn = opts.publishedOn;
-  this.image = opts.image;
-  this.body = opts.body;
-}
-
-Project.all = [];
-//Going to refactor the code below
-Project.prototype.toHtml = function() {
-  var theTemplate = Handlebars.compile($('#article-template').text());
-
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  this.body = marked(this.body);
-
-  return theTemplate(this);
-};
-
-Project.loadAll = function(projectData) {
-  console.log('Entering Loadall');
-  projectData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  projectData.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-//semicolons might be throwing it off
-
-
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
-
-Project.fetchAll = function() {
-  // DEBUG
-  console.log('fetch started');
-  if (localStorage.projectData) {
-    // When rawData is already in localStorage,
-    // we can load it by calling the .loadAll function,
-    // and then render the index page (using the proper method on the articleView object).
-    // DEBUG
-    console.log('localStorage exists. Loading...');
-    Project.loadAll(JSON.parse(localStorage.projectData));
-    portfolioView.initIndexPage(); //probably need to make sure this works in the portfolioView page.
-  } else {
-    console.log('localStorage does not exist. Loading...');
-    $.getJSON('../data/projectData.json', function(data) {
-      Project.loadAll(data);
-      localStorage.setItem('projectData', JSON.stringify(data));
-      portfolioView.initIndexPage();
-    }).fail(function(jqXHR, textStatus, errorThrown){
-      console.log(textStatus);
-    });
+(function(module) {
+  function Project (opts) {
+    this.title = opts.title;
+    this.about = opts.about;
+    this.category = opts.category;
+    this.source = opts.source;
+    this.sourceUrl = opts.sourceUrl;
+    this.publishedOn = opts.publishedOn;
+    this.image = opts.image;
+    this.body = opts.body;
   }
-};
+
+  Project.all = [];
+  Project.prototype.toHtml = function() {
+    var theTemplate = Handlebars.compile($('#article-template').text());
+
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = marked(this.body);
+
+    return theTemplate(this);
+  };
+
+  Project.loadAll = function(projectData) {
+    console.log('Entering Loadall');
+    projectData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    Project.All = projectData.map(function(ele) {
+      console.log(ele);
+      return new Project(ele);
+    });
+  };
+
+  Project.fetchAll = function(callback) {
+    console.log('fetch started');
+    if (localStorage.projectData) {
+      console.log('localStorage exists. Loading...');
+      Project.loadAll(JSON.parse(localStorage.projectData));
+      callback();
+      // Project.numImages();
+    } else {
+      console.log('localStorage does not exist. Loading...');
+      $done = $.getJSON('data/projectData.json', function(projectData) {
+        Project.loadAll(projectData);
+        localStorage.projectData = JSON.stringify(projectData);
+        callback();
+      }).done(function(){
+        // Project.numImages();
+        Project.allProjects();
+        Project.numWordsByProject();
+      });
+    }
+  };
+
+//Chaining together a map and reduce so that I can get a rough estimate of word count.
+  Project.numWords = function() {
+    return Project.all.map(function(project) {
+      var newArr = Project.body.split(' ');
+      return newArr.length;
+    })
+    .reduce(function(a, b) {
+      return a + b;
+    },0);
+  };
+// Creating an array of unique project titles
+  Project.allProjects = function() {
+    return Project.all.map(function(project) {
+      return project.title;
+    })
+    .filter(function(project, arr, index) {
+      return arr.indexOf(project) === index;
+    });
+  };
+
+  Project.getProjectWords = function() {
+    return Project.all.filter(function(project) {
+      return Project.title === title;
+    })
+    .map(function(projectTitle) {
+      return projectTitle.body;
+    })
+    .map(function(body) {
+      return body.split(' ').length;
+    })
+    .reduce(function(acc, b) {
+      return acc + b;
+    }, 0);
+  };
+
+  Project.numWordsByProject = function() {
+    return Project.allProjects().map(function(project) {
+      return {
+        name: title,
+        numWords: Project.getProjectWords(project)
+      };
+    });
+  };
+
+  module.Project = Project;
+})(window);
--- scripts/portfolioView.js
+++ scripts/portfolioView.js
@@ -1,116 +1,141 @@
-var portfolioView = {};
+(function(module) {
 
-portfolioView.createFilters = function() {
-  var templateScript = $('#filter').html();
+  var portfolioView = {};
 
-  var theTemplate = Handlebars.compile(templateScript);
+  portfolioView.createFilters = function() {
+    $('#projects').each(function() {
+      if (!$(this).hasClass('template')) {
+        var val = $(this).find('h2').text();
+        var optionTag = '<option value="' + val + '">' + val + '</option>';
+        $('#title-filter').append(optionTag);
+        val =$(this).data('category');
+        optionTag = '<option value="' + val + '">' + val + '</option>';
 
-  var filters = [{
-    id: 'title-filter',
-    defaultVal:'-- Filter by Title --'},
-    { id: 'category-filter',
-    defaultVal:'-- Filter by Categories --' }];
-
-  filters.forEach(function(filter) {
-    $('#filters').append(theTemplate(filter));
-  });
-
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).find('h2').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      $('#title-filter').append(optionTag);
-
-      val =$(this).data('category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-
-      if($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
+        if($('#category-filter option[value="' + val + '"]').length === 0) {
+          $('#category-filter').append(optionTag);
+        }
       }
-    }
-  });
-};
+    });
+  };
+    // var templateScript = $('#filter').html();
+    //
+    // var theTemplate = Handlebars.compile(templateScript);
+    //
+    // var filters = [{
+    //   id: 'title-filter',
+    //   defaultVal:'-- Filter by Title --'},
+    //   { id: 'category-filter',
+    //   defaultVal:'-- Filter by Categories --' }];
+    //
+    // filters.forEach(function(filter) {
+    //   $('#filters').append(theTemplate(filter));
+    // });
 
-portfolioView.handleTitleFilter = function() {
-  $('#title-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $('article[data-title="' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-    $('#category-filter').val('');
-  });
-};
-portfolioView.categoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this). val()) {
-      $('article').hide();
-      $('article[data-category="' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-    $('#title-filter').val('');
-  });
-};
+  portfolioView.handleTitleFilter = function() {
+    $('#title-filter').on('change', function() {
+      if ($(this).val()) {
+        $('article').hide();
+        $('article[data-title="' + $(this).val() + '"]').fadeIn();
+      } else {
+        $('article').fadeIn();
+        $('article.template').hide();
+      }
+      $('#category-filter').val('');
+    });
+  };
 
-portfolioView.hideView = function() {
-  $('.navContent').on('click', '.tab', function() {
-    $('.tab-content').hide();
+  portfolioView.handleCategoryFilter = function() {
+    $('#category-filter').on('change', function() {
+      if ($(this). val()) {
+        $('article').hide();
+        $('article[data-category="' + $(this).val() + '"]').fadeIn();
+      } else {
+        $('article').fadeIn();
+        $('article.template').hide();
+      }
+      $('#title-filter').val('');
+    });
+  };
 
-    $('#' + $(this).data('content')).show();
-  });
+  portfolioView.handleMainNav = function() {
+    $('.navContent').on('click', '.tab', function(e) {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
 
-  $('.navContent .tab:first').click();
-};
+    $('.navContent .tab:first').click();
+  };
 
-portfolioView.adjustNavDisplay = function() {
-  $('.icon-menu').on('click', function() {
-    $('.navContent ul').fadeToggle('fast');
-  });
-};
+  // portfolioView.hideView = function() {
+  //   $('.navContent').on('click', '.tab', function() {
+  //     $('.tab-content').hide();
+  //
+  //     $('#' + $(this).data('content')).show();
+  //   });
+  //
+  //   $('.navContent .tab:first').click();
+  // };
 
-portfolioView.initNewProjectPage = function() {
-  $('.tab-content').show();
-  $('#export-field').hide();
-  $('#project-json').on('focus', function() {
-    this.select();
-  });
+  portfolioView.adjustNavDisplay = function() {
+    $('.icon-menu').on('click', function() {
+      $('.navContent ul').toggle('fast');
+    });
+  };
 
-  $('#new-form').on('change', 'input, textarea', portfolioView.create);
-};
+  portfolioView.initNewProjectPage = function() {
+    $('.tab-content').show();
+    $('#export-field').hide();
+    $('#project-json').on('focus', function() { //look into this...
+      this.select();
+    });
 
-portfolioView.create = function() {
-  var project;
-  $('#projects').empty();
+    $('#new-form').on('change', 'input, textarea', portfolioView.create);
+  };
 
-  project = new Project({
-    title: $('#project-title').val(),
-    about: $('#project-about').val(),
-    category: $('#project-category').val(),
-    source: $('#project-source').val(),
-    sourceUrl: $('#project-sourceUrl').val(),
-    publishedOn: $('#project-publishedOn').length ? util.today() : null,
-    image: $('#project-image').val(),
-    body: $('#project-body').val()
-  });
+  portfolioView.create = function() {
+    var project;
+    $('#projects').empty();
 
-  $('#projects').append(article.toHtml());
+    project = new Project({
+      title: $('#project-title').val(),
+      category: $('#project-category').val(),
+      source: $('#project-source').val(),
+      sourceUrl: $('#project-sourceUrl').val(),
+      publishedOn: $('#project-publishedOn').length ? util.today() : null,
+      body: $('#project-body').val()
+    });
 
-  $('#export-field').show();
-  $('#article-json').val(JSON.stringify(article) + ', ');
-};
+    $('#projects').append(project.toHtml());
+    $('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
 
-portfolioView.initIndexPage = function() {
-  Project.all.forEach(function(a) {
-    $('#projects').append(a.toHtml());
-  });
+    $('#export-field').show();
+    $('#project-json').val(JSON.stringify(article) + ', ');
+  };
 
-  portfolioView.createFilters();
-  portfolioView.handleTitleFilter();
-  portfolioView.categoryFilter();
-  portfolioView.hideView();
-  portfolioView.adjustNavDisplay();
-};
+  portfolioView.initIndexPage = function() {
+    Project.all.forEach(function(a) {
+      $('#projects').append(a.toHtml());
+    });
+    Project.numWords();
+    portfolioView.createFilters();
+    portfolioView.handleTitleFilter();
+    portfolioView.handleCategoryFilter();
+    portfolioView.handleMainNav();
+    portfolioView.adjustNavDisplay();
+  };
+
+  // portfolioView.initAdminPage = function() {
+  //   var theTemplate = Handlebars.compile($('#write').text());
+  //
+  //   Project.numWordsByProject().forEach(function(stat) {
+  //     $('.cody-stats').append(template(stat));
+  //   });
+  //
+  //   $('#project-stats .projects').text(Project.all.length);
+  //   $('#project-stats .words').text(Project.numWords());
+  // };
+
+  module.portfolioView = portfolioView;
+})(window);
